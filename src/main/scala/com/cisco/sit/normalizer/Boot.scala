1f50cc3b22e64d83f85c13431251cc81a6294fc9
Dima Statz <dima.statz@cloudlock.com>
2017-12-21 23:09:29 +0200

    add data reader actor

--- src/main/scala/com/cisco/sit/normalizer/Boot.scala
+++ src/main/scala/com/cisco/sit/normalizer/Boot.scala
@@ -1,8 +1,27 @@
 package com.cisco.sit.normalizer
 
+import akka.actor._
+import akka.util.Timeout
+import java.util.concurrent.TimeUnit
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration.FiniteDuration
+import com.typesafe.scalalogging.LazyLogging
+import com.cisco.sit.normalizer.actors.ActorDataReader
 
-object Boot {
+object Boot extends LazyLogging {
+  private implicit val ec = ExecutionContext.global
+  private implicit val timeout = Timeout(FiniteDuration(5, TimeUnit.SECONDS))
+
   def main(args: Array[String]): Unit = {
-    println("starting normalizer")
+    logger.info("starting normalizer")
+
+    val system = ActorSystem("normalizer")
+    val reader = system.actorOf(Props(new ActorDataReader(null)))
+    logger.info(s"reader created ${reader.hashCode()}")
+
+    sys addShutdownHook {
+      logger.info("normalizer is shutting down")
+      system.terminate()
+    }
   }
 }
---
+++ src/test/scala/Workspace.sc
@@ -0,0 +1,17 @@
+import org.apache.commons.lang3.exception.ExceptionUtils
+
+import scala.util._
+
+def f() = {
+  throw new Exception("some exception")
+}
+
+
+def f1() = {
+  Try(f()) match {
+    case Failure(x) => println(ExceptionUtils.getStackTrace(x))
+  }
+}
+
+
+f1()
